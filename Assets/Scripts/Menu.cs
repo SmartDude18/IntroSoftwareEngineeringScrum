47a0caacd99a7b3de667285bb6010285cdaf99b5
CSipes05 <157405016+CSipes05@users.noreply.github.com>
2026-02-24 09:05:07 -0700

    Added UI to testing scene

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] AudioSource menuMusic;
+    private void Start()
+    {
+        if (!menuMusic.isPlaying)
+        {
+            menuMusic.Play();
+        }
+    }
     public void StartGame()
     {
-
+        menuMusic.Stop();
+        SceneManager.LoadScene("Sound Testing");
     }
     public void ExitGame()
     {
-
+        Application.Quit();
     }
 }
